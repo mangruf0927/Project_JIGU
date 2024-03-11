b0288a9ff4d7898df73a9b71bd4951a8ada65103
mangruf <pinky0927@naver.com>
2024-03-11 16:35:08 +0900

    Feat : 플레이어 틀 구현

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(inputVec); 
+    }
+} 
